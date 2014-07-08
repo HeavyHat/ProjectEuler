1a8c5224ec882514e78dde11275811c1737c6319
HeavyHat <Joshua.Langley.1995@gmail.com>
2014-07-08 12:01:29 +0100

    Fix for ex8

--- ex8.java
+++ ex8.java
@@ -9,37 +9,16 @@ public class ex8
     return description;
   }
 
-  //Gets the product of a digit string
-  public static int digitProduct(String digitsString)
-  {
-    int currentResult = 1;
-    for(int currentDigit = 0; currentDigit < digitsString.length(); currentDigit++)
-      currentResult *= Character.digit(digitsString.charAt(currentDigit),10);
-    return currentResult;
-  }
-
   public static void main(String[] args)
   {
-    int frontPointer = 0;
-    int backPointer = Integer.parseInt(args[0]);
+    int length = Integer.parseInt(args[0]);
     String fullNumber = args[1];
-    long largestSoFar = 0;
-    String productNumbers = "";
-    while(backPointer < fullNumber.length())
-    {
-      if(digitProduct(fullNumber.substring(frontPointer, backPointer)) > largestSoFar)
-      {
-        largestSoFar = digitProduct(fullNumber.substring(frontPointer,backPointer));
-        productNumbers = fullNumber.substring(frontPointer, backPointer);
-      }
-      frontPointer++;
-      backPointer++;
-    }
-    for(int digit= 0; digit < productNumbers.length(); digit++)
-    {
-      System.out.print(productNumbers.charAt(digit));
-      if(digit != productNumbers.length() - 1)
-        System.out.print(" X ");
+    long largestSoFar = -1;
+    for (int frontPointer = 0; frontPointer + length <= fullNumber.length(); frontPointer++) {
+      long product = 1;
+      for (int currentPointer = 0; currentPointer < length; currentPointer++)
+        product *= fullNumber.charAt(frontPointer + currentPointer) - '0';
+      largestSoFar = Math.max(product, largestSoFar);
     }
     System.out.println();
     System.out.println(largestSoFar);
