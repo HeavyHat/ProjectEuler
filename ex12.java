9c37ef2bdbaf328b2a40e799541713644e7da118
HeavyHat <Joshua.Langley.1995@gmail.com>
2014-07-09 09:15:03 +0100

    Finished ex12.java

--- ex12.java
+++ ex12.java
@@ -1,7 +1,45 @@
 public class ex12 extends ProjectEulerSolution
 {
+
   public ex12()
   {
-    description = "Highly divisible triangular number."
+    description = "Highly divisible triangular number.";
+  }
+
+  //returns the number of factors this number has
+  public int divisibility(int number)
+  {
+    //count of divisors found
+    int count = 0;
+    //use the sqrt for smaller numbers
+    int end = (int)Math.sqrt(number);
+    for (int currentFactor = 1; currentFactor < end; currentFactor++) {
+      if (number % currentFactor == 0)
+        count += 2;
+    }
+    if (end * end == number)
+      count++;
+    return count;
+  }
+
+  @Override
+  public void run(String[] args)
+  {
+    //the number of factors the triangular number should have
+    int desiredDivisibility = Integer.parseInt(args[0]);
+    int lastNumber = 1;
+    //the number in the series
+    int numberInSeries = 2;
+    boolean found = false;
+    while(!found)
+    {
+      if(divisibility(lastNumber) >= desiredDivisibility)
+        found=true;
+      else
+        lastNumber = lastNumber + numberInSeries;
+      numberInSeries++;
+    }
+    //print result
+    System.out.println(lastNumber);
   }
 }
