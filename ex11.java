c0293b1b1b7f8de7c3aab66ec0a6dc310d471b50
HeavyHat <Joshua.Langley.1995@gmail.com>
2014-07-09 08:40:37 +0100

    Finished ex 11

--- ex11.java
+++ ex11.java
@@ -10,23 +10,30 @@ public class ex11 extends ProjectEulerSolution
     description = "Largest product in a grid";
   }  
 
+  //returns true if travelling ADJACENT tiles results in being in bounds
+  //NOTE: deltax/y should be in the range -1 <= x <= 1
   public boolean isInBounds(int x, int y, int deltaX, int deltaY)
   {
     return x + deltaX * ADJACENT >= 0 && x + deltaX * ADJACENT < GRID_SIZE
             && y + deltaY * ADJACENT >= 0 && y + deltaY * ADJACENT < GRID_SIZE;
   }
 
+  //calculates the product of of an adjacent in a given direction
+  //NOTE: deltax/y should be in the range -1 <= x <= 1
   public int adjacentProduct(int x, int y, int deltaX, int deltaY)
   {
     int currentProduct = 1;
     int xOffset = 0;
     int yOffset = 0;
-    if(!isInBounds(x,y,deltaX,deltaY))
+    // if not in bounds or not in a valid direction return -1
+    if(!isInBounds(x,y,deltaX,deltaY) || deltaX == 0 && deltaY == 0)
       return -1;
     
-    for(int counter = 0; xOffset < ADJACENT; xOffset += deltaX, yOffset += deltaY)
+    for(int counter = 0; counter < ADJACENT; xOffset += deltaX
+                                            ,yOffset += deltaY
+                                            , counter++)
       currentProduct *= grid[x + xOffset][y + yOffset];
-
+    //System.out.println(currentProduct);
     return currentProduct;
   }
 
@@ -36,12 +43,17 @@ public class ex11 extends ProjectEulerSolution
     int largestProduct = 0;
     //fill the grid with the command line arguments
     for(int argumentNumber = 0; argumentNumber < args.length; argumentNumber++)
-      grid[argumentNumber % GRID_SIZE][argumentNumber / GRID_SIZE] = Integer.parseInt(args[argumentNumber]);
+      grid[argumentNumber % GRID_SIZE][argumentNumber / GRID_SIZE] 
+        = Integer.parseInt(args[argumentNumber]);
+
+    //tries every tile in every direction and passes it to the adjacent 
     for(int x = 0; x < GRID_SIZE; x++)
       for(int y = 0; y < GRID_SIZE; y++)
         for(int xDirection = -1; xDirection <= 1; xDirection++)
           for(int yDirection = -1; yDirection <= 1; yDirection++)
-            largestProduct = Math.max(adjacentProduct(x,y,xDirection,yDirection), largestProduct);
+            largestProduct= Math.max(adjacentProduct(x,y,xDirection,yDirection)
+                                     , largestProduct);
+    //Print the result
     System.out.println(largestProduct);
   }
 
