ab50095c383bf86377d99b66aef144f84d3dc49c
HeavyHat <Joshua.Langley.1995@gmail.com>
2014-07-11 10:50:06 +0100

    Started ex16 Power digit sum from first impressions requires bit shifting to be a good solution

---
+++ ex16.java
@@ -0,0 +1,13 @@
+public class ex16 extends ProjectEulerSolution
+{
+  public ex16()
+  {
+    description = "Power digit sum";
+  }
+
+  @Override 
+  public void run(String[] args)
+  {
+    
+  }
+}
